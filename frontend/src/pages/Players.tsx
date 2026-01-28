4c0186ebfb8f0d751495ea5b4fa96e96861bfaa4
Francisco Peralta <fran.mper@gmail.com>
2026-01-27 21:46:40 -0300

    fix build

--- frontend/src/pages/Players.tsx
+++ frontend/src/pages/Players.tsx
@@ -6,7 +6,6 @@ import { useAvailability } from '../hooks/useAvailability';
 import { useMatches } from '../hooks/useMatches';
 import { Button } from '../components/ui/button';
 import { Input } from '../components/ui/input';
-import { Card, CardContent } from '../components/ui/card';
 import { Alert, AlertDescription } from '../components/ui/alert';
 import {
   Dialog,
